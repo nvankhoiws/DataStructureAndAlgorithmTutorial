9c66da7155192892ce63dfb3f76ee90f7eb95605
Nguyen Van Khoi <nvankhoiws@gmail.com>
2017-11-11 09:01:50 +0700

    add new file

---
+++ src/core/com/udemy/fundamental/Stack.java
@@ -0,0 +1,24 @@
+package core.com.udemy.fundamental;
+
+public class Stack {
+    public static boolean push(String s) {
+
+        return true;
+    }
+
+    public static boolean pop(String s) {
+        return true;
+    }
+
+    public static Integer peak() {
+        return null;
+    }
+
+    public static Integer size(){
+        return null;
+    }
+
+    public static void setCapacity(){
+
+    }
+}
