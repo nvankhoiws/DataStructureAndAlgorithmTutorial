8652fc7a437d52537c526821d010fb9b6a47a5c6
Nguyen Van Khoi <nvankhoiws@gmai.com>
2017-11-13 17:52:01 +0700

    Add class SinglyLinkedList

---
+++ src/core/com/udemy/fundamental/linkedList/SinglyLinkedList.java
@@ -0,0 +1,62 @@
+package core.com.udemy.fundamental.linkedList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Khoi.NguyenVan@nttdata.com on 11/13/20179:16 AM.
+ */
+public class SinglyLinkedList {
+    Node first;
+
+    public void add(Node newNode){
+        Node tmp = first.getNext();
+        while(tmp != null) {
+            if (tmp.getNext() == null){
+
+            } else {
+
+            }
+            tmp = tmp.getNext();
+        }
+    }
+
+    public void remove(Node removedNode){
+
+    }
+
+    public void size(){
+
+    }
+
+    public boolean isEmpty(){
+
+    }
+
+    public boolean contains(Node node) {
+
+    }
+
+    public int indexOf(Node node){
+
+    }
+
+    public Node get(int index){
+
+    }
+
+    public void set(int index, Node node){
+
+    }
+
+    public void add(int index, Node node){
+
+    }
+
+    public void clear(){
+
+    }
+
+    List list = new ArrayList()
+
+}
