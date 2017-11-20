dfdea4d451245776624deb0b4882485b4ce32a4e
Nguyen Van Khoi <nvankhoiws@gmai.com>
2017-11-20 18:07:02 +0700

    Add file

---
+++ src/core/com/udemy/fundamental/linearSearch/BinarySearch.java
@@ -0,0 +1,14 @@
+package core.com.udemy.fundamental.linearSearch;
+
+/**
+ * Created by Khoi.NguyenVan@nttdata.com on 11/20/20175:40 PM.
+ */
+
+/**
+ * BinarySearch works on sorted Array
+ */
+public class BinarySearch {
+
+    public static void binarySearch(int[] array, )
+
+}
